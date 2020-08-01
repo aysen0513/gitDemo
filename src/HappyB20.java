6cf41906b9e941ba4d7bf5095f3f835aa9eebb3d
aysen0513 <aysenbayserke@hotmail.com>
2020-08-01 12:14:11 -0400

    update HappyB20

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -2,7 +2,7 @@ public class HappyB20 {
 
     public static void main(String[] args) {
 
-        System.out.println("HappyB20");
+        System.out.println("HelloB20 class");
         int a = 2;
         int b = 3;
         System.out.println(a+b);
